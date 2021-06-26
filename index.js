19735ca049c3da9fa9e9b412d894da1cb86342d4
1nch MAN <76808801+1nchpp@users.noreply.github.com>
2021-06-26 22:42:38 +0000

    AAAAAAAAh

---
+++ index.js
@@ -0,0 +1,38 @@
+const io = require("socket.io-client");
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const urls = []
+
+const socket = io("wss://pinger-hub.1nchpp.repl.co", {
+  reconnection: true,
+  reconnectionAttempts: 10000,
+  reconnectionDelay: 6000,
+  reconnectionDelayMax: 100000,
+});
+
+function send() {
+  urls.forEach(axios.get)
+}
+
+setInterval(send, 10000)
+
+socket.on('ping', async m => {
+  urls.push(m)
+})
+
+socket.on('connect', function() {
+  console.log("e")
+});
+
+socket.on('disconnect', () => {
+  setTimeout(() => {
+    console.log("r")
+  }, 6000)
+})
+
+app.get("/", async (req, res) => {
+  res.end(`hi`)
+})
+
+app.listen(4000)
